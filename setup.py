09f38a6c25026009b0b93bacd22a8a5bcfe4625f
Markus Patzold <patzold@outlook.com>
2023-06-01 09:53:14 +0000

    043

--- setup.py
+++ setup.py
@@ -58,11 +58,11 @@ INSTALL_REQUIRES = get_install_requirements(INSTALL_REQUIRES, ALT_INSTALL_REQUIR
 
 setup(
     name="imgaug",
-    version="0.4.2",
+    version="0.4.3",
     author="Alexander Jung",
     author_email="kontakt@ajung.name",
     url="https://github.com/marcown/imgaug",
-    download_url="https://github.com/marcown/imgaug/archive/0.4.2.tar.gz",
+    download_url="https://github.com/marcown/imgaug/archive/0.4.3.tar.gz",
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
